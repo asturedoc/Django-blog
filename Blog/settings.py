98bff1b0aaf99cc0b9f08f42b53f30edfda63189
brandaab <brex201alex@mail.com>
2021-06-02 09:55:00 -0700

    first commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'asturedoc2021@gmail.com'
-EMAIL_HOST_PASSWORD = 'redoc@2021'
+EMAIL_HOST_USER = 'EMAIL user'
+EMAIL_HOST_PASSWORD = 'User password'
